b7d67b549228817dbc79697d66cea6c5ee9d57e6
yumeng.5158 <yumeng.5158@bytedance.com>
2020-06-21 23:38:51 +0800

    更新代码

---
+++ js/common.js
@@ -0,0 +1,69 @@
+/**
+ * 默认配置
+ */
+;(function ($) {
+    $(function () {
+        // 切换语言
+        $('.menu-news-main li').on('click', function () {
+            var lan = $(this).attr('lan');
+            $.cookie('tf-lan', lan, { expires:  30});
+            renderDom();
+        });
+        // 返回顶部
+        $('.tf-backtop').on('click', function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 300);
+        })
+    });
+})(jQuery);
+// 滑动事件，给header添加样式
+window.onscroll = function() {
+    //为了保证兼容性，这里取两个值，哪个有值取哪一个
+    //scrollTop就是触发滚轮事件时滚轮的高度
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop) {
+        $('.tf-header').addClass('scroll')
+    }
+    else {
+        $('.tf-header').removeClass('scroll')
+    }
+}
+// 通过语言，获取翻译
+function renderDom() {
+    // 获取所有带data-lan标签的元素，用于翻译
+    var allLan = $('[data-lan]');
+    var allLanPlace = $('[data-lan-placeholder]');
+    // 从cookie里面记录当前语言
+    var lan =  $.cookie('tf-lan') || 'cn';
+    // 存储缓存30天
+    $.cookie('tf-lan', lan, { expires:  30});
+    // 从配置中读取语言列表
+    var config = lanConfig[lan] || {};
+    // 获取元素
+    $('.menu-news-main li').removeClass('active');
+    var lanLi = $('.menu-news-main li[lan='+lan+']');
+    lanLi.addClass('active')
+    $('.lan-name').html(lanLi.html());
+    allLan.each(function(i) {
+        var lanText = $(this).data('lan').split('.')
+        // 判断是否为数据
+        var trueText = getTrueText(lanText, 0, config)
+        $(this).html(trueText);
+    });
+    allLanPlace.each(function(i) {
+        var lanText = $(this).data('lan-placeholder').split('.')
+        // 判断是否为数据
+        var trueText = getTrueText(lanText, 0, config)
+        $(this).attr('placeholder', trueText);
+    });
+}
+
+function getTrueText(obj, index, params ) {
+    if (obj.length === index + 1) {
+        return params[obj[index]] || ''
+    }
+    params = params[obj[index]];
+    index++;
+    return getTrueText(obj, index, params)
+}
---
+++ js/lan.js
@@ -0,0 +1,188 @@
+/**
+ * 语言配置
+ */
+
+var lanConfig = {
+    cn: {
+        navProducts: '产品',
+        navSolutions: '解决方案',
+        navContact: '合作咨询',
+        navAboutUs: '关于我们',
+        bannerTitle: '低成本<br/>快速搭建交易所',
+        bannerSubTitle: 'TradeFlex<br/>全球领先的区块链技术服务商，提供安全、稳定、可靠、覆盖全球的一站式服务',
+        bannerBtn: '立即合作',
+        bannerSend: '发送',
+        title1: '产品与服务',
+        title2: 'TradeFlex 平台优势',
+        title3: '立即咨询',
+        desc3: '了解开设交易所业务与相关运营全部信息，TradeFlex会保护您的隐私',
+        productTitle1: '现货交易系统',
+        productTitle2: '合约交易系统',
+        productTitle3: 'OTC交易系统',
+        productTitle4: '流动性提供系统',
+        productTitle5: '钱包对接服务',
+        productDesc1: '高性能撮合引擎 / 系统安全可靠 / 交易对上限自由 / 专业交易 / 多平台支持 / 邀请返佣系统',
+        productDesc2: '多种合约交易类型 / 金融级风控 / 交易对自定义 / 多平台支持 / 开放API接口',
+        productDesc3: '多种法币支持 / 安全认证 / 快速交易 / 多平台支持',
+        productDesc4: '多种交易类型 / 流动性机制 / 智能聚合深度 / 特色对接方式',
+        productDesc5: '安全稳定 / 多币种对接 / 支持多交易系统',
+        productItem11: {
+            title: '高性能撮合引擎',
+            desc: '内存撮合，平台订单统一撮合，毫秒级订单撮合时间'
+        },
+        productItem12: {
+            title: '系统安全可靠',
+            desc: '系统资金风控，异常行为监控，清算透明化'
+        },
+        productItem13: {
+            title: '交易对上限自由',
+            desc: '支持多种Token协议，随时上币'
+        },
+        productItem14: {
+            title: '专业交易',
+            desc: '提供K线技术分析工具，支持策略委托，支持API下单'
+        },
+        productItem15: {
+            title: '多平台支持',
+            desc: '支持Web端，安卓，IOS移动端应用'
+        },
+        productItem16: {
+            title: '邀请返佣系统',
+            desc: '支持多级返佣，自定义返佣手续费比例，助推平台快速发展'
+        },
+        productItem21: {
+            title: '多种合约交易类型',
+            desc: '支持双向开仓，交易品种自定义，支持逐仓和全仓模式'
+        },
+        productItem22: {
+            title: '系统安全可靠',
+            desc: '通过交易制度风控，系统风控，实时监控交易状态，保证平台交易安全'
+        },
+        productItem23: {
+            title: '交易对自定义',
+            desc: '支持任何交易对上限，可自定义杠杆'
+        },
+        productItem24: {
+            title: '多平台支持',
+            desc: '支持Web端，安卓，IOS移动端应用'
+        },
+        productItem25: {
+            title: '开放API接口',
+            desc: '提供全方位API接口，方便对接量化与做市商系统'
+        },
+        productItem31: {
+            title: '多种法币支持',
+            desc: '支持RMB，USD，EUR等多种法币交易'
+        },
+        productItem32: {
+            title: '安全认证',
+            desc: '多重用户KYC认证，多等级商家认证，保证交易双方资金安全'
+        },
+        productItem33: {
+            title: '快速交易',
+            desc: '支持银行卡，微信，支付宝等多种支付方式，无缝接入商户的各种业务场景'
+        },
+        productItem34: {
+            title: '多平台支持',
+            desc: '支持Web端，安卓，IOS移动端应用，方便商家随时处理交易'
+        },
+        productItem41: {
+            title: '多种交易类型',
+            desc: '提供现货、合约和期权流动性解决方案'
+        },
+        productItem42: {
+            title: '流动性机制',
+            desc: '可以自定义点差和数量，拥有远近盘机制，减少波动带给交易所的压力'
+        },
+        productItem43: {
+            title: '智能聚合深度',
+            desc: '聚合头部交易所盘后和深度，提供主流币种流动性'
+        },
+        productItem44: {
+            title: '自主token深度',
+            desc: '提供市值策略和工具，为自主研发token提供流动性'
+        },
+        productItem51: {
+            title: '安全稳定',
+            desc: '多年钱包对接维护经验，高可靠性钱包技术，确保交易所资产安全稳定'
+        },
+        productItem52: {
+            title: '多币种对接',
+            desc: '提供多币种钱包对接，减少交易所维护工作量'
+        },
+        productItem53: {
+            title: '智能聚合深度',
+            desc: '支持现货，OTC和合约交易系统钱包定制，让钱包使用简单高效'
+        },
+        advantageItem1: {
+            title: '安全可靠',
+            desc: '金融级架构，全方位多重风控系统，风控异常实时报警'
+        },
+        advantageItem2: {
+            title: '性能强大',
+            desc: '纯内存异步撮合系统支持100000单/秒，引擎集群化'
+        },
+        advantageItem3: {
+            title: '数据自持',
+            desc: '用户数据私有化，实现无感知平移，手握流量'
+        },
+        advantageItem4: {
+            title: '灵活管理',
+            desc: '强大管理后台支持，支持自定义配置，便于数据管理'
+        },
+        name: '姓名',
+        email: '邮箱',
+        footerS1: '数字资产交易解决方案',
+        footerS2: '流动性系统解决方案',
+        footerS3: '钱包系统解决方案',
+        footerS4: '关于',
+        solutionsTitle: '在模块化的产品架构智商，TradeFlex平台<br/>针对不同的交易所应用场景，交付高质量、<br/>易落地的创新解决方案',
+        solutionsItem1: {
+            title: '数字资产交易解决方案',
+            desc: '依托创新的产品设计理念和业界顶尖技术，提供高性价比交易系统部署搭建和业务组合推荐服务，为客户提供量身定制的交易系统。'
+        },
+        solutionsItem2: {
+            title: '流动性系统解决方案',
+            desc: '针对初创交易所痛点，提供集量化策略交易、市值和交易流动性交易为一体的综合性一站式流动性解决方案。'
+        },
+        solutionsItem3: {
+            title: '钱包系统解决方案',
+            desc: '凭借多年区块链领域技术深耕和经验积累，推出以安全为核心、接入快成本低、并结合业务、运营、管理等模式的全生态钱包解决方案。'
+        },
+        solutionsInfo1: {
+            item1: '多种产品自由组合，提供现货，合约，OTC等多种交易模块，根据客户要求提供一体化解决方案',
+            item2: '支持私有化和SaaS部署，简单快速完成整套交易系统的部署',
+            item3: '通过冷热钱包结合、私钥二次加密、数据加密传输、硬件离线存储、多层次多维度安全风控等方式打造银行级安全系统。',
+            item4: '引入主流交易所的专线数据，如历史参考、实时行情、盘口深度等，7*24全天候提供高品质的金融数据服务，确保价格精准、权威、透明。',
+            item5: '量身定制的代理返佣系统，与交易平台高度集成，支持自定义代理级别、返佣规则，完备的会员管理功能和多层次佣金结算体系，多维度报表体系。',
+            item6: '多样化营销推广策略，7*24小时交易与技术支持，为客户提供性能稳定、安全可靠的一体化解决方案。'
+        },
+        solutionsInfo2: {
+            item1: '整合交易所深度和盘口，为中小交易所提供稳定仿真流量、价格区间和市场趋势',
+            item2: '引用专线数据，聚合深度与盘口，以降低对冲风险获取最优价格，提供流动性支持，减轻运营压力及成本',
+            item3: '灵活多变的平台接入方式，7*24小时的交易支持，只为给客户提供更优质、更稳定的安全可靠解决方案。'
+        },
+        solutionsInfo3: {
+            item1: '支持多种钱包形态，多样运营模式，多类金融产品，币种支持丰富，可根据需求高度定制，方便运营需要',
+            item2: '钱包私钥冷热分离，采用多种加密手段、操作权限分级等多层次多维度安全风控策略，确保资产绝对安全',
+            item3: '根据行业积累经验，简化了繁琐的操作流程，实现高效管理数字资产',
+            item4: '提供一体化解决方案，快速部署、标准化API接口快速接入、7*24小时客服支持，让企业用户专注于自身行业的运营与管理'
+        }
+    },
+    en: {
+        navProducts: 'Products',
+        navSolutions: 'Solutions',
+        navContact: 'Contact',
+        navAboutUs: 'About Us',
+        bannerTitle: 'For starters<br/>lorem ipsum isnt',
+        bannerSubTitle: 'For starters, lorem ipsum isn’t just some wacky<br/>text-thingy with no meaning whatsoever. These<br/> words have been derived from',
+        bannerBtn: 'GET STARTED',
+        bannerSend: 'SEND',
+        name: 'Name',
+        email: 'Email',
+    },
+    // 韩语
+    ko: {
+
+    }
+}
